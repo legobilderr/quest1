1f15967ff0cd039510f37917ff2bf0faab698429
legobilderr <pomidorsenior28@gmail.com>
2020-10-05 13:23:11 +0300

    test

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-$test='hello';
-echo $test;
+$array1=array(1,2,4,412,32,142,231,2341,23,32,41);
+function maximum($some_array){
+    $max_v=0;
+    for ($i = 0;$i<count($some_array);$i++){
+        if($some_array[$i]>$max_v){
+            $max_v=$some_array[$i];
+        }
+    }
+    return $max_v;
+}
 
+$answer=maximum($array1);
+echo $answer;
 ?>
