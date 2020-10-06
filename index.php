46660248f03cc9a94801514d24bf9e589c8b74a2
legobilderr <pomidorsenior28@gmail.com>
2020-10-06 17:11:31 +0300

    3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$array1=array(1,2,4,412,32,142,231,2341,23,32,41);
+$array1=array(1,2,4,412,32,142,231,2341,23,32);
 function maximum($some_array){
     $max_v=0;
     for ($i = 0;$i<count($some_array);$i++){
@@ -28,4 +28,39 @@ for ($i = 0; $i < $len ; $i++){
 foreach ($array1 as $varib){
     echo $varib.',';
 }
+
+echo '<br/>';
+
+
+
+function b_serch ($some_array,$serch_obj){
+    $len = count($some_array);
+    $meedle = floor($len/2);
+    $step=$meedle/2;
+    for ($i = 0; $i < $len;$i++){
+        if ($serch_obj==$some_array[$meedle]){
+            $answer=$meedle+1;
+            echo  'его номер в списке : ';
+            echo $answer;
+            return ;            
+        }
+        elseif ($serch_obj<$some_array[$meedle] || $meedle>=$len ){
+            $meedle=floor($meedle-$step);
+            $step=$step/2;
+            // echo $meedle;
+            // echo ' шаг назат </br>';
+        }
+        else {
+            $meedle=ceil($meedle+$step);
+            $step=$step/2;
+            // echo $meedle;
+            // echo ' шаг вперёд </br>';
+        }
+    }
+    echo 'этого элемента нет в списке ';
+}
+b_serch($array1,23);
+
+
+
 ?>
